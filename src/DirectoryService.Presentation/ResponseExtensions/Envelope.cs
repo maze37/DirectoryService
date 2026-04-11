2d7929af737fc0fb8d6881c3c1c5b9f8c3008231
maze37 <denzfaceit@gmail.com>
2026-04-11 22:17:45 +0300

    feat: добавил Result, Envelope

---
+++ src/DirectoryService.Presentation/ResponseExtensions/Envelope.cs
@@ -0,0 +1,28 @@
+﻿using Shared.Result;
+
+namespace DirectoryService.Presentation.ResponseExtensions;
+
+public record Envelope
+{
+    public object? Result { get; }
+
+    public ErrorList? Errors { get; }
+
+    public DateTimeOffset TimeGenerated { get; }
+
+    private Envelope(object? result, ErrorList? errors)
+    {
+        Result = result;
+        Errors = errors;
+        TimeGenerated = DateTimeOffset.UtcNow;
+    }
+  
+    public static Envelope Ok(object? result = null) =>
+        new(result, null);
+
+    public static Envelope Error(ErrorList errors) =>
+        new(null, errors);
+    
+    public static Envelope Error(Error error) =>
+        new(null, error.ToErrorList());
+}
