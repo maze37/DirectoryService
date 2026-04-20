9e112711f76a27d76139e18946c7429876e520e6
maze37 <denzfaceit@gmail.com>
2026-04-20 15:40:05 +0300

    del: удалил тесты

--- tests/DirectoryService.ApiTests/DirectoryServiceApiCollection.cs
+++
@@ -1,9 +0,0 @@
-using Xunit;
-
-namespace DirectoryService.ApiTests;
-
-[CollectionDefinition(Name)]
-public sealed class DirectoryServiceApiCollection : ICollectionFixture<DockerComposeFixture>
-{
-    public const string Name = "directory-service-api";
-}
